7d2c71c589dfeef2d38c48d17e2d46f66985579b
haya-alwarthan <111225040+haya-alwarthan@users.noreply.github.com>
2023-09-04 15:13:28 +0300

    Adding global fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,27 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+
     extend: {
+      colors: {
+      
+        'primary': 'var(--color-primary)',
+        'primary-hover': 'var(--color-primary-hover)',
+        'secondary': 'var(--color-secondary)',
+        'secondary-hover': 'var(--color-secondary-hover)',
+        'tertiary' :'var(--color-tertiary)',
+        'tertiary-hover' :'var(--color-tertiary-hover)'},
       textColor:{
         skin:{
           primary: 'var(--color-primary)',
           secondary:  'var(--color-secondary)',
+          tertiary:  'var(--color-tertiary)',
         }
+      },
+      fontFamily: {
+  
+        tenor: ['Tenor Sans'],
+        joseph: ['Josefin Sans']
       }
 
 
