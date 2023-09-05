b4a73ac15450dffccee16ece88a92b63158facb6
haya-alwarthan <111225040+haya-alwarthan@users.noreply.github.com>
2023-09-05 06:39:21 +0300

    Completed Hero feature

---
+++ src/routes/Contact/+page.svelte
@@ -0,0 +1,4 @@
+<section id='contact' class="flex justify-center">
+    <div class="left"></div>
+    <div class="right"></div>
+</section>
--- src/routes/Hero/+page.svelte
+++ src/routes/Hero/+page.svelte
@@ -1,17 +1,19 @@
 <script>
-    import Navbar from "./Navbar.svelte";
-    import Intro from "./Intro.svelte";
+	import Navbar from './Navbar.svelte';
+	import Intro from './Intro.svelte';
 </script>
 
-<section id="hero" class="relative w-full h-screen mx-auto bg-hero bg-no-repeat  bg-cover bg-center items-center flex">
-    <Navbar/>
-    <Intro/>
+<section
+	id="hero"
+	class="relative w-full h-screen mx-auto bg-hero bg-no-repeat bg-cover bg-center items-center flex"
+>
+	<Navbar />
+	<Intro />
 </section>
+
 <style>
-
-.bg-hero{
-    background-image: url('./src/lib/assets/images/hero-bg.png');
-    background-color: black;
-}
-
+	.bg-hero {
+		background-image: url('./src/lib/assets/images/hero-bg.png');
+		background-color: black;
+	}
 </style>
