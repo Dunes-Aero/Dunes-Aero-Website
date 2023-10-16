5bda6c67c6a7cc74697b8abebf5aad290b077538
Hashem Bahamdoon <104528991+HashemBahamdoon@users.noreply.github.com>
2023-10-16 17:05:16 +0300

    carousel adv added

--- frontend/src/routes/DunesAeroADV/components/items.svelte
+++ frontend/src/routes/DunesAeroADV/components/items.svelte
@@ -1,11 +1,10 @@
 <script>
-	import firstDist from '$lib/assets/images/Our Distinguishment 3.png';
-	import secondDist from '$lib/assets/images/Our Distinguishment 1.png';
-	import thirdDist from '$lib/assets/images/Our Distinguishment 4.png';
-	import fourthDist from '$lib/assets/images/Our Distinguishment 2.png';
+	import Carousel from 'svelte-carousel';
+
+	import { browser } from '$app/environment';
 </script>
 
-<div
+<!-- <div
 	class="flex sm:flex-wrap items-center justify-center p-6 md:flex-row space-x-16 md:space-x-4 2xl:space-x-10"
 >
 	<div
@@ -19,6 +18,38 @@
 		<img src={thirdDist} alt="" class="w-24 md:w-28 2xl:w-56 h-auto p-2 2xl:p-10" />
 		<img src={fourthDist} alt="" class="w-28 md:w-36 2xl:w-64 h-auto pt-6 2xl:p-10" />
 	</div>
+</div> -->
+<div class=" bg-primary">
+	<!-- <button class="pre-btn bg-primary-hover hover:bg-secondary-hover rounded" on:click={move}
+	on:click={() => move('prev')}>
+	  
+	  <img src="src\lib\assets\images\right.svg" alt="">
+	</button>
+	<button class="nxt-btn bg-primary-hover hover:bg-secondary-hover rounded" on:click={move}
+	on:click={() => move('next')}>
+	  
+	  <img src="src\lib\assets\images\right.svg" alt="">
+	</button> -->
+	{#if browser}
+		<div class=" bg-primary">
+			<Carousel particlesToShow={5} particlesToScroll={2}>
+				<img src="src\lib\assets\images\adv1.svg" class="product-thumb px-10 " height="250" width="250"  alt="1" />
+				
+				<img src="src\lib\assets\images\adv2.svg" class="product-thumb px-10 " height="250" width="250" alt="1" />
+				<img src="src\lib\assets\images\adv3.svg" class="product-thumb px-10 " height="250" width="250" alt="3" />
+
+				<img src="src\lib\assets\images\adv4.svg" class="product-thumb px-10  " height="250" width="250" alt="4" />
+
+				<img src="src\lib\assets\images\adv5.svg" class="product-thumb px-10 " height="250" width="250" alt="5"/>
+
+				<img src="src\lib\assets\images\adv6.svg" class="product-thumb px-10 " height="250" width="250" alt="6" />
+
+				<img src="src\lib\assets\images\adv7.svg" class="product-thumb px-10 " height="250" width="250" alt="7" />
+
+				<img src="src\lib\assets\images\adv8.svg" class="product-thumb px-10 " height="250" width="250" alt="8" />
+			</Carousel>
+		</div>
+	{/if}
 </div>
 
 <style>
